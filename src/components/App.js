f20ec96048799d1b77e8f1d67a3bbc161556f875
Shripad bhalearo <96540872+shripad1312@users.noreply.github.com>
2024-03-12 16:00:00 +0530

    first submission in react

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,32 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//         <
+//     </div>
+//   )
+// }
 
+class App extends Components{
+  constructor(props){
+    super(props);
+    this.state={count:0};
+  }
+   
+  increment=()=>{
+  const curr=this.state.count;
+  this.setState({count:curr+1});
+  }
+  render(){
+    return (
+          <div>
+         <p>Button clicked {this.state.count} times</p>
+           <button onClick={this.increment}>click me</button>  
+          </div>
+        ) 
+  }
+}
 export default App
