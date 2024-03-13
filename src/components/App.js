9ea7756beee0df6fbda5e17db4da2fab317cc8ae
Shripad bhalearo <96540872+shripad1312@users.noreply.github.com>
2024-03-13 17:40:18 +0530

    changed app to cpp

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React,{Component} from "react";
 import {useState } from "react";
 import './../styles/App.css';
 import Comps from "./Counting";
-const App = () => {
+const Cpp = () => {
 
  
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import Cpp from "./components/App";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Cpp />, document.getElementById("root"));
