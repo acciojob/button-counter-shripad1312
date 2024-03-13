4fdc9531f50cc1ee61219a49ebebc8301b4577d8
Shripad bhalearo <96540872+shripad1312@users.noreply.github.com>
2024-03-13 17:18:13 +0530

    using function based state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React,{Component} from "react";
+import {useState } from "react";
 import './../styles/App.css';
-import Counter from "./components/Counting";
+// import Counter from "./components/Counting";
 const App = () => {
+const [count,setCount]=useState(0)
+  const add=()=>{
+  setCount(count+1);
+  }
   return (
     <div>      
-       <Counter/>
+       {/* <Counter/> */}
+       <p>Button clicked {state} times</p>
+       <button onClick={add}>Click me</button>
     </div>
   )
 }
